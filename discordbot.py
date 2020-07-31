2b2b74c9359b18bd2d981a329a412282ff901c92
Thomas Anderson <knarfytrebil@gmail.com>
2020-08-01 07:17:50 +0900

    welcome message

--- discordbot.py
+++ discordbot.py
@@ -7,18 +7,22 @@ import random
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+WELCOME_MESSAGE = """
+Welcome {0.mention} to {1.name}! 
+Please wait here before someone give you a permission!
+"""
 
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    error_msg = WELCOME_MESSAGE.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
     
 @bot.event
 async def on_member_join(member):
     guild = member.guild
     if guild.system_channel is not None:
-        to_send = 'Welcome {0.mention} to {1.name}!'.format(member, guild)
+        to_send = ''.format(member, guild)
         await guild.system_channel.send(to_send)
 
 @bot.command()
