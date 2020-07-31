40380d2f42d6deef7ebb85ad2bc6e9d674aa0c03
Thomas Anderson <knarfytrebil@gmail.com>
2020-07-31 21:29:19 +0900

    implement roll

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,8 @@ from discord.ext import commands
 import os
 import traceback
 
+import random
+
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
@@ -12,10 +14,9 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
+async def roll(ctx):
+    rnd = random.randrange(1, 100)
+    await ctx.send(rnd)
 
 bot.run(token)
